4e0119f6d6b8a85b28779f1750d568f771593496
raynecloudy <rayne@raynec.dev>
2025-06-25 23:58:57 -0700

    yes

--- bin.js
+++ bin.js
@@ -1,3 +1,17 @@
-#!/usr/bin/env node
-
-console.log(process.argv);
+#! /usr/bin/env node
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+(async () => {
+    const argv = await yargs(hideBin(process.argv)).argv;
+    process.stdout.write("\x1b[0m");
+    if (argv._.length === 0) {
+        console.log("\x1b[1mscratch.js CLI\x1b[0m");
+        console.log();
+        console.log("Commands:");
+        console.log("   create   Create a new scratch.js project");
+        console.log();
+        console.log("Mrow!");
+        console.log("=^..^=");
+    }
+    process.stdout.write("\x1b[0m");
+})();
