760af96cbc2bbc64be1326894f7fe11cc5780420
raynecloudy <contact@raynec.dev>
2025-09-08 19:30:25 -0700

    logout and token

--- scratch.js
+++ scratch.js
@@ -1,5 +1,7 @@
 import { default as fetch } from "node-fetch";
 
+let token = null;
+
 export const login = (username, password) => new Promise(async (resolve, reject) => {
   const response = await fetch("https://scratch.mit.edu/accounts/login/", {
     credentials: "include",
@@ -21,12 +23,25 @@ export const login = (username, password) => new Promise(async (resolve, reject)
   const data = (await response.json())[0];
 
   if (response.ok) {
-    resolve({
-      token: data.token,
-      tries: data.num_tries
-    });
+    token = data.token;
+    resolve("Logged in successfully");
   } else {
     reject(data.msg);
   }
 });
 
+export const logout = async () => {
+  if (!token) return;
+  console.log(await fetch("https://scratch.mit.edu/accounts/logout/", {
+    credentials: "include",
+    method: "POST",
+    headers: {
+      "Cookie": "scratchcsrftoken=scratchjs; scratchlanguage=en",
+      "Origin": "https://scratch.mit.edu",
+      "Referer": "https://scratch.mit.edu/",
+      "X-CSRFToken": "scratchjs",
+      "X-Requested-With": "application/x-www-form-urlencoded",
+      "Content-Type": "application/json"
+    }
+  }));
+};
